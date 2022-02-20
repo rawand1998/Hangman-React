23fede4c8fb99602591fd86ad9e9ecb72d857e09
rawand1998 <rawandgaradh@gmail.com>
2022-02-20 17:02:35 +0200

    components

---
+++ src/Components/Word.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Word() {
+  return (
+ 
+    <div class="word" id="word"></div>
+   
+  )
+}
+
+export default Word
---
+++ src/Components/WrongWordLetter.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function WrongWordLetter() {
+  return (
+    <div class="wrong-letters-container">
+    <div id="wrong-letters"></div>
+  </div>
+  )
+}
+
+export default WrongWordLetter
